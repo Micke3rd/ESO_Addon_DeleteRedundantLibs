3621dcc3695de0cdb6197ec708990e937e74a429
Micke <peter.tschaikowsky@gmx.de>
2019-09-24 20:56:13 +0200

    - better description in which folder tool is searching - updating variable OptionalDependsOn in *.txt file, with the deleted libs (if not already listed)

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-//</ script >
 
 
 public class Program
@@ -46,25 +45,64 @@ public class Program
 
         Console.WriteLine();
         foreach (var e in folders)
-            try
+        {
+            Console.WriteLine();
+            ModifyAddon(e);
+        }
+    }
+
+    public static void ModifyAddon(DirectoryInfo dir)
+    {
+        var root = dir.Parent.Parent;
+        var txt = root.GetFiles(root.Name + ".txt");
+
+        if (txt.Count() > 0)
+        {
+            var target = txt.First().FullName;
+            var source = File.ReadAllLines(target).ToList();
+            var alreadyLinked = new HashSet<string>();
+            var s1 = "## DependsOn:"; var s2 = "## OptionalDependsOn:";
+            int? idxOptionalDependsOn = null;
+
+            foreach (var row in source)
             {
-                e.Delete(true);
-                Console.WriteLine("deleted: " + e.FullName);
+                if (!row.Contains(s1) && !row.Contains(s2))
+                    continue;
+                if (row.Contains(s2))
+                    idxOptionalDependsOn = source.IndexOf(row);
+                foreach (var e3 in row.Split(new[] { s1, s2, " " }, StringSplitOptions.RemoveEmptyEntries))
+                    alreadyLinked.Add(e3);
             }
-            catch (Exception ex)
+            if (!alreadyLinked.Contains(dir.Name))
             {
-                Console.Write(ex.Message + " # " + e.FullName);
+                if (idxOptionalDependsOn == null)
+                {
+                    source.Insert(0, s2);
+                    idxOptionalDependsOn = 0;
+                }
+
+                source[(int)idxOptionalDependsOn] += " " + dir.Name;
+                Console.WriteLine("modifying file: " + target);
+                Console.WriteLine(source[(int)idxOptionalDependsOn]);
+                File.WriteAllLines(target, source);
             }
+
+        }
+        try
+        {
+            dir.Delete(true);
+            Console.WriteLine("deleted: " + dir.FullName);
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.Message + " # " + dir.FullName);
+        }
     }
 
     static List<DirectoryInfo> Collect()
     {
-
-        var ass = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Assembly.Location;
-        //var ass = @"Z:\O\Dokumente\Elder Scrolls Online\live\AddOns\DeleteRedundantLibs\Program.cs";
-        var dir = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent;
-        Console.WriteLine("running in " + dir.FullName);
-
+        var dir = GetAddonPath();
+        Console.WriteLine("searching in " + dir.FullName);
 
         var addons = dir.GetDirectories().Select(e => e.Name).ToList();
 
@@ -76,5 +114,28 @@ public class Program
                         result.Add(lib);
 
         return result;
+    }
+    public static DirectoryInfo GetAddonPath()
+    {
+        var addons = "AddOns".ToLower();
+        //var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+        var dir = new DirectoryInfo(@"C:\Users\Test\Documents\Elder Scrolls Online\live\AddOns\DeleteRedundantLibs");
+        try
+        {
+            if (dir.Name.ToLower().Equals(addons))
+                return dir;
+
+            while (dir.Parent != null)
+            {
+                dir = dir.Parent;
+                if (dir.Name.ToLower().Equals(addons))
+                    return dir;
+            }
+            throw new System.NotSupportedException("code cant reach here usually.");
+        }
+        catch
+        {
+            throw new Exception("working folder " + dir.FullName + " is not located somewhere in the AddOn folder (live/pts).");
+        }
     }
 }
